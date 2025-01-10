c068f3b256293c89989539fca07d692e7d95d425
Ali Safaroff <ali@example.com>
2025-01-10 12:08:59 +0400

    New

--- MVC(GncyYldz)/FirstMVC/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FirstMVC.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal stock { get; set; }
-    }
-}
